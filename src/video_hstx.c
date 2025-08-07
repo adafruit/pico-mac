13dfdf6849e2fb865aab8a13b8ca9185b5e3427b
Liz <liz@adafruit.com>
2025-08-07 16:23:19 -0400

    not rot, try tmds

--- src/video_hstx.c
+++ src/video_hstx.c
@@ -35,9 +35,9 @@
 // ----------------------------------------------------------------------------
 // DVI constants
 
-#define TMDS_CTRL_00 0x354u
+#define TMDS_CTRL_00 0x154u
 #define TMDS_CTRL_01 0x0abu
-#define TMDS_CTRL_10 0x154u
+#define TMDS_CTRL_10 0x354u
 #define TMDS_CTRL_11 0x2abu
 
 #define SYNC_V0_H0 (TMDS_CTRL_00 | (TMDS_CTRL_00 << 10) | (TMDS_CTRL_00 << 20))
@@ -163,7 +163,7 @@ void    video_init(uint32_t *framebuffer) {
 
     size_t pixels_per_word = 32;
     size_t words_per_line = REAL_DISP_WIDTH / pixels_per_word;
-    uint8_t rot = 25; // 24 + color_depth;
+    uint8_t rot = 24; // 24 + color_depth;
     size_t shift_amount = 31; // color_depth % 32;
 
     size_t command_word = 0;
